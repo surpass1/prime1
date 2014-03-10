ecdb4fb868b08f6aebbe79ba866a00bca90d6c7d
surpass1 <paulkawalya@yahoo.com>
2014-03-10 16:36:16 +0300

    prime first

---
+++ prime.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void main()
+{
+	int i,n,r=0;
+	
+	printf(“Enter any no: ”);
+	scanf(“%d”,&n);
+	for(i=2;i<=n-1;i++)
+	{
+		if(n%i==0)
+		r=1;
+		break;
+	}
+	
+	if(r==0)
+	{
+	printf(“prime no”);
+	}
+	else
+	{
+	printf(“Not prime”);
+	}
+}
+
